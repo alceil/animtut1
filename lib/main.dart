ffc2fdffd654a2a91649fc741e09d6489f2ba5ec
alceil <ashishshajisrampickal@gmail.com>
2020-03-11 20:47:58 +0530

    munjiya jeevitham

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,20 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   home:MyHomePage()
 ));
-
 class MyHomePage extends StatelessWidget {
+  final items=List<ListItem>.generate(1200, (i)=>i%6==0?HeadingItem("Heading $i"):MessageItem("Sender $i","Meassage body $i"));
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('item deletion'),),
-        body:GridView.count(
-            crossAxisCount: 2,
-            children: List.generate(100, (i){
-              return Center(
-                child: Text('Item $i'),
-              );
-            }
-            )
-
-        )
+      appBar: AppBar(
+        title: Text("Damn"),
+      ),
+      body: ListView.builder(itemCount:items.length,itemBuilder: (context,index){
+        return ();
+      }),
     );
   }
 }
+
+
 
