67ca1e14ce321f7f3118f884979b33ba56db901c
alceil <ashishshajisrampickal@gmail.com>
2020-03-11 14:26:56 +0530

    used the snackbar and added form

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,49 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text(appTitle),
         ),
+        body: CustomForm(),
+        
       ),
     );
   }
 }
+class CustomForm extends StatefulWidget {
+  @override
+  _CustomFormState createState() => _CustomFormState();
+}
+
+class _CustomFormState extends State<CustomForm> {
+  final _formKey=GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            validator: (value){
+              if(value.isEmpty){
+                return 'Enter some text kid';
+              }
+              else
+                return null;
+            },
+          ),
+          RaisedButton(
+            onPressed: (){
+              if(_formKey.currentState.validate())
+                {
+                  Scaffold.of(context).showSnackBar(SnackBar(content: Text('Processing data'),));
+
+                }
+            },
+            child: Text('Submit'),
+          )
+
+        ],
+      ),
+    );
+  }
+}
+
 
