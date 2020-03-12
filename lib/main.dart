b08d4b09575e7a48cba028d99c9893d2f31eb2c8
alceil <ashishshajisrampickal@gmail.com>
2020-03-12 09:47:39 +0530

    added sliver app bar

--- lib/main.dart
+++ lib/main.dart
@@ -10,43 +10,19 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
 class MyHomePage extends StatelessWidget {
-  final items=List<ListItem>.generate(1200, (i)=>i%6==0?HeadingItem("Heading $i"):MessageItem("Sender $i","Meassage body $i"));
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Damn"),
-      ),
-      body: ListView.builder(itemCount:items.length,itemBuilder: (context,index){
-        final item=items[index];
-        if (item is HeadingItem)
-        {
-        return ListTile(
-          title: Text(item.heading),
-
-        );
-        }
-        else if(item is MessageItem)
-        {
-          return ListTile(
-            title: Text(item.sender),
-            subtitle:Text(item.body) ,
-          );
-        }
-      }
-      ),
+      body:CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            title: Text(),
+          )
+        ],
+      ) ,
     );
   }
 }
-abstract class ListItem{}
 
-class HeadingItem extends ListItem{
-  final String heading;
-  HeadingItem(this.heading);
-}
-class MessageItem extends ListItem{
-  final String sender,body;
-  MessageItem(this.sender,this.body);
-}
+
