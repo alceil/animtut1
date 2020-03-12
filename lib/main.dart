8b3a02c90c8faf22ecec63ecb0c2af31902287d5
alceil <ashishshajisrampickal@gmail.com>
2020-03-12 13:57:59 +0530

    added album

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,57 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp(
+import 'package:http/http.dart' as http;
 
-));
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/':(context)=>MyHomePage(),
-        '/second':(context)=>DetailsPage(),
-      },
+Future<Album> fetchAlbum() async
+{
+  final response = await http.get('https://jsonplaceholder.typicode.com/albums/1');
+  if(response.statusCode==200)
+  {
+    return Album.fromJson(json.decode(response.body));
+  }
+  else
+    {
+      throw Exception('Failed to load a album');
+    }
+}
+class Album{
+  final int userId;
+  final int id;
+  final String title;
+  Album({this.userId,this.id,this.title});
+  factory Album.fromJson(Map <String,dynamic> json)
+  {
+    return Album(
+      userId:json['userId'],
+        id:json['id'],
+        title:json['title'],
     );
   }
 }
-class MyHomePage extends StatelessWidget {
+void main()=>runApp(MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+
+class _HomeState extends State<Home> {
+ Future<Album> futureAlbum;
+ @override
+  void initState() {
+    super.initState();
+    futureAlbum=fetchAlbum();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Long list'),
+        title: Text('Fetch album data'),
       ),
-      body:Center(
-        child: RaisedButton(onPressed: ()
-        {
-          Navigator.pushNamed(context, '/second');
-        },
-          child: Text('Open Route'),
-        ),
-      ) ,
     );
   }
 }
-class DetailsPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Second Route'),),
-      body: Center(
-        child: RaisedButton(onPressed: ()
-        {
-          Navigator.pop(context);
-        },
-          child: Text('Go Back'),
-        ),
-      ),
-    ) ;
-  }
-}
-
-
