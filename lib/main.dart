202ac9dd526e80a0c38c4bcd15ed6b80d828e3d5
alceil <ashishshajisrampickal@gmail.com>
2020-03-12 20:06:53 +0530

    added class photo

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,112 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<Album> fetchAlbum() async {
-  final http.Response response =
-  await http.get('https://jsonplaceholder.typicode.com/albums/1');
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return Album.fromJson(json.decode(response.body));
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
-}
-Future<Album> deleteAlbum(String id) async {
-  final http.Response response = await http.delete('https://jsonplaceholder.typicode.com/albums/$id',
-      headers:<String,String>
-      {
-        'Content-Type':'application/json;charset=UTF-8',
-      } );
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return Album.fromJson(json.decode(response.body));
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
+Future<Album> fetchAlbum() async{
+  final response =http.get('https://jsonplaceholder.typicode.com/albums/1',headers: {HttpHeaders.authorizationHeader:"blah"});
+  final responseJson = json.decode(response.body);
+  return Album.fromJson(responseJson);
 }
 
 
-class Album {
+Future<Album> createAlbum(String title) async {
+  final http.Response response = await http.post(
+    'https://jsonplaceholder.typicode.com/albums',
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'title': title,
+    }),
+  );
+
+  if (response.statusCode == 201) {
+    return Album.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to create album.');
+  }
+}
+class photo{
   final int id;
   final String title;
-
-  Album({ this.id, this.title});
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-
-      id: json['id'],
-      title: json['title'],
+  final String thumbnailUrl;
+  photo({this.id,this.title,this.thumbnailUrl});
+  factory photo.fromJson(Map<String,dynamic> json)
+  {
+    return photo(
+        id: json['id'],
+        title: json['title'],
+        thumbnailUrl: json['thumbnailUrl']
     );
   }
 }
 
-void main() => runApp(MyApp());
+
+
+}
+
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget {
   MyApp({Key key}) : super(key: key);
 
   @override
-  _MyAppState createState() => _MyAppState();
+  _MyAppState createState() {
+    return _MyAppState();
+  }
 }
 
 class _MyAppState extends State<MyApp> {
-  Future<Album> futureAlbum;
-
-  @override
-  void initState() {
-    super.initState();
-    futureAlbum = fetchAlbum();
-  }
+  final TextEditingController _controller = TextEditingController();
+  Future<Album> _futureAlbum;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fetch Data Example',
+      title: 'Create Data Example',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Fetch Data Example'),
+          title: Text('Create Data Example'),
         ),
-        body: Center(
-          child: FutureBuilder<Album>(
-            future: futureAlbum,
+        body: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8.0),
+          child: (_futureAlbum == null)
+              ? Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                controller: _controller,
+                decoration: InputDecoration(hintText: 'Enter Title'),
+              ),
+              RaisedButton(
+                child: Text('Create Data'),
+                onPressed: () {
+                  setState(() {
+                    _futureAlbum = createAlbum(_controller.text);
+                  });
+                },
+              ),
+            ],
+          )
+              : FutureBuilder<Album>(
+            future: _futureAlbum,
             builder: (context, snapshot) {
-              if(snapshot.connectionState==ConnectionState.done)
-                {
-                  if (snapshot.hasData) {
-                    return Column(
-                      children: <Widget>[
-                        Text('${snapshot.data?.title??'Deleted'}'),
-                        RaisedButton(
-                          onPressed: (){setState(() {
-                            futureAlbum=deleteAlbum(snapshot.data.id.toString());
-                          });},
-                          child: Text('Delete Data'),
-                        )
-                      ],
-                    );
-                  }
-                }else if (snapshot.hasError) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data.title);
+              } else if (snapshot.hasError) {
                 return Text("${snapshot.error}");
               }
 
-              // By default, show a loading spinner.
               return CircularProgressIndicator();
             },
           ),
